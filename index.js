2528bf9854eaa108156e84601ba6be2c420c1f00
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-18 21:20:12 -0400

    add menu and event pages

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("index");
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
