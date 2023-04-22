b0e5e1b9fc2cde197828c3e770784185a763a87b
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-22 10:42:31 -0400

    getting initial access to data from back end via promises

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,14 +1,24 @@
 const test = document.querySelector("#test");
+const promiseTest = document.querySelector("#promise-test");
 test.textContent = `teeeest`;
 
-async function getDrinkMenu() {
-  const response = await fetch("/drinks");
-  const menu = await response.json();
-  console.log(menu);
+async function getMenu(menu) {
+  try {
+    const response = await fetch(`/${menu}`);
+    const data = await response.json();
+    return Promise.resolve(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-try {
-  getDrinkMenu();
-} catch (err) {
-  console.log(err);
+getMenu("drinks").then(
+  (data) => (promiseTest.innerText = renderCoffeeNames(data))
+);
+
+function renderCoffeeNames(data) {
+  const names = [];
+  const coffees = data.category.coffees;
+  coffees.forEach((coffee) => names.push(coffee.name));
+  return names.join("\n");
 }
