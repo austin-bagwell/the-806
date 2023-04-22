b3020bd06243966d0c6b0c580ab4093dcaae20ad
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-22 16:23:22 -0400

    use fetch + clientside JS to render HTML from JSON

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,6 +1,6 @@
-const test = document.querySelector("#test");
 const promiseTest = document.querySelector("#promise-test");
-test.textContent = `teeeest`;
+
+const main = document.querySelector(".main");
 
 async function getMenu(menu) {
   try {
@@ -12,13 +12,54 @@ async function getMenu(menu) {
   }
 }
 
-getMenu("drinks").then(
-  (data) => (promiseTest.innerText = renderCoffeeNames(data))
+getMenu("drinks").then((data) =>
+  main.insertAdjacentElement("beforeend", renderMenuSectionSkeleton(data))
 );
 
-function renderCoffeeNames(data) {
-  const names = [];
-  const coffees = data.category.coffees;
-  coffees.forEach((coffee) => names.push(coffee.name));
-  return names.join("\n");
+function renderListItems(menu, category) {
+  const items = menu.categories[`${category}`];
+  const html = items.map((item) => {
+    return `<li class="no-bullets">
+        <span class="font-bold-md">${item.name}... </span>
+        <span>${item.description}</span>
+        </li>`;
+  });
+  return html.join("");
 }
+
+// TODO
+// how to render through all categories in the JSON?
+// is that even what I want to do?
+// will need different layout for the flavors and the loose leaf teas
+function renderMenuSectionSkeleton(menu) {
+  const section = document.createElement("section");
+  const heading = document.createElement("h2");
+  const list = document.createElement("ul");
+
+  const categories = menu.categories;
+  console.log(categories);
+  const category = "coffees";
+
+  heading.innerText = "Coffees";
+  const menuItems = renderListItems(menu, category);
+  list.insertAdjacentHTML("beforeend", menuItems);
+
+  section.insertAdjacentElement("beforeend", heading);
+  section.insertAdjacentElement("beforeend", list);
+
+  return section;
+}
+
+/* TODO
+  Need to get a function that renders all this, for each category present in the menu JSON
+  
+  <section>
+    <h2 class="section-heading">Coffees</h2>
+    <ul id="name of the category here ie coffees/espressos/etc">
+      <li> all the list items here </li>
+    </ul>
+  </section>
+
+
+
+*/
